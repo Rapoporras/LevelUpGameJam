c5f1f654f855f557c25d6beff443533cd01e7b49
Lost <fernandocastromartinez123@gmail.com>
2024-07-06 17:24:15 +0200

    d

--- Assets/Scripts/RouteSelector.cs
+++ Assets/Scripts/RouteSelector.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RouteSelector : MonoBehaviour
 {
 
-    public GameManager manager;
+    //public GameManager manager;
+    public int level;
     public void NegacionistR()
     {
-        manager.bandoJugador = false;
+        PlayerPrefs.SetInt("Negacion", 1);
+        //manager.bandoJugador = false;
+        SceneManager.LoadSceneAsync(level);
     }
 
     public void CientificR()
     {
-        manager.bandoJugador = true;
+
+        PlayerPrefs.SetInt("Negacion", 0);
+        //manager.bandoJugador = true;
+
+        SceneManager.LoadSceneAsync(level);
     }
 }
